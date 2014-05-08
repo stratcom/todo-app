4c46b4c85afc4a660e71c10f8a4e6c2dbd13b418
stratcom <mhstratton@gmail.com>
2014-05-07 21:32:46 -0600

    starting adding comments to .js file

---
+++ script.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+// set up listeners
+	//keystroke
+	function setListeners(){
+		$("#new-todo").keyup(function(e){
+			if(e.keystroke==13){do stuff here});
+		});
+	}
+
+
+
+	//checkmark=click
+	//complete all - click
+	//remove all completed
+	//delete-click
+	//hover
+	//all/active/complted - click
+
+
+
+
+
+.ke
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
