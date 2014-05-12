04af9463e3a410a6895e6b1a9b3f266383b612f8
stratcom <mhstratton@gmail.com>
2014-05-12 16:58:42 -0600

    added local storage code, still not working

--- script.js
+++ script.js
@@ -52,8 +52,48 @@ $(document).ready(function(){
  				$('#todo-list li:last-child label').text(entry);
  				$('#todo-list li:last-child').attr('data-id',id);
 				$('#new-todo').val('');
-	}			
+	}
 
+	function uuid() {
+  			var i, random;
+			var uuid = '';
+
+		for (i = 0; i < 32; i++) {
+			random = Math.random() * 16 | 0;
+			if (i === 8 || i === 12 || i === 16 || i === 20) {
+				uuid += '-';
+			}
+			uuid += (i === 12 ? 4 : (i === 16 ? (random & 3 | 8) : random)).toString(16);
+		}
+		return uuid;
+  	}
+  	
+  		function saveEntry(entry) {
+		var entry = $('#new-todo').val();
+		var id = uuid();
+		var todoEntry = {
+			'id':id,
+			'name':entry,
+			}
+		localStorage.setItem('Todos', JSON.stringify(todoEntry));
+		
+		/*        todos = [];
+		    } else {
+		        // Parse the serialized data back into an array of objects
+		        todos = JSON.parse(localStorage.getItem('todos'));*/
+		}
+
+
+
+
+
+
+
+  	//}				
+
+  	// $(function() {
+  	// 'id':id,
+  	//		'name':entry
 	
 			
 	// finish local storage
