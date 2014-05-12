6373e9c8f94a85072f7aeb2b08e9551cac9cdea9
stratcom <mhstratton@gmail.com>
2014-05-12 13:12:54 -0600

    adding local storage code

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 $(document).ready(function(){
 
-	// set up listeners
+	var todos = '';
+	function fromStorage() {
+		// get entries from localstorage if they exist.
+		// populate the list with entries and ids.
+		if(localStorage && localStorage.length > 0) {
+
+			// run populateList to insert all the entries
+			// populateList();
+		} else {
+			var a = [];
+			localStorage.setItem('todos',JSON.stringify(a));
+		}
+		// setListners after the list has been built.
+		setListeners();
+	}	
+
 	function setListeners(){
 		//keystroke
 		$("#new-todo").keyup(function(e){
@@ -10,18 +25,45 @@ $(document).ready(function(){
 	}		    	
 			
 	function checkInput(e) {
+		// if enter key
 		if(e.keyCode == 13){
-			insertEntry();
+			//no longer insertEntry();
+			//save the entry to localStorage
+			saveEntry();
 			console.log("enter key pressed");
+			//if esc key			
 		}
 		else if (e.keyCode == 27) {
 			//clear input box
 			console.log("esc key pressed");
 		};
 	}
+	// build list based on the contents of localStorage
+	function populateList() {
+		for (var i = localStorage.length - 1; i >= 0; i--) {
+			//localStorage[i]
+			console.log("Entries in localStorage");
+		};
+	} 
+
+	function insertEntry(entry, id) {
+		 		var entry = $('#new-todo').val();
+ 				$('.template li').clone().appendTo('#todo-list');
+ 				$('#todo-list li:last-child label').text(entry);
+ 				$('#todo-list li:last-child').attr('data-id',id);
+				$('#new-todo').val('');
+	}			
 
 	
 			
+
+	//checkmark=click
+	//complete all - click
+	//remove all completed
+	//delete-click
+	//hover
+	//all/active/complted - click
+
 							
 
 });
@@ -33,9 +75,14 @@ $(document).ready(function(){
 
 
 
-	//checkmark=click
-	//complete all - click
-	//remove all completed
-	//delete-click
-	//hover
-	//all/active/complted - click
+
+
+
+
+
+
+
+
+
+
+
